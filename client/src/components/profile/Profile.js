6288b2dc6c44d33956e434c244cda3e8c5d3d94c
Kittichote Kamalapirat <shane@Kittichotes-MacBook-Air.local>
2021-12-10 11:39:36 +0700

    spinner

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,13 +1,13 @@
-import React, { Fragment, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Spinner from '../layout/Spinner';
-import { getProfileById } from '../../actions/profile';
-import { getPosts } from '../../actions/post';
-import ProfileTop from './ProfileTop';
-import PostItem from '../posts/PostItem';
-import ProfileAbout from './ProfileAbout';
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/SpinnerIcon";
+import { getProfileById } from "../../actions/profile";
+import { getPosts } from "../../actions/post";
+import ProfileTop from "./ProfileTop";
+import PostItem from "../posts/PostItem";
+import ProfileAbout from "./ProfileAbout";
 
 const Profile = ({
   getProfileById,
@@ -15,7 +15,7 @@ const Profile = ({
   profile: { profile, loading },
   auth,
   post: { posts },
-  match
+  match,
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
@@ -31,8 +31,8 @@ const Profile = ({
           {!profile ? (
             <Fragment>
               <h1>Sorry, this user doesn't create a profile yet</h1>
-              <Link to='/'>
-                <i class='fas fa-chevron-left'></i>Back to Home
+              <Link to="/">
+                <i class="fas fa-chevron-left"></i>Back to Home
               </Link>
             </Fragment>
           ) : (
@@ -44,26 +44,26 @@ const Profile = ({
               {auth.isAuthenticated &&
                 auth.loading === false &&
                 auth.user._id === profile.user._id && (
-                  <Link to='/edit-profile' className='btn btn-dark'>
+                  <Link to="/edit-profile" className="btn btn-dark">
                     Edit Profile
                   </Link>
                 )}
 
-              <div className='profile-section'>
+              <div className="profile-section">
                 <ProfileTop profile={profile} />
                 <ProfileAbout profile={profile} />
               </div>
-              <div className='look-down'>
-                <i className='fas fa-chevron-down fa-2x'></i>
-                <i className='fas fa-chevron-down fa-2x relative'></i>
+              <div className="look-down">
+                <i className="fas fa-chevron-down fa-2x"></i>
+                <i className="fas fa-chevron-down fa-2x relative"></i>
               </div>
               <h2>Posts from {profile.user.name}</h2>
-              {posts.filter(post => post.user === profile.user._id).length >
+              {posts.filter((post) => post.user === profile.user._id).length >
               0 ? (
                 <Fragment>
                   {posts
-                    .filter(post => post.user === profile.user._id)
-                    .map(mypost => (
+                    .filter((post) => post.user === profile.user._id)
+                    .map((mypost) => (
                       <PostItem key={mypost._id} post={mypost} />
                     ))}
                 </Fragment>
@@ -84,14 +84,11 @@ const Profile = ({
 Profile.propTypes = {
   getProfileById: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   profile: state.profile,
   auth: state.auth,
-  post: state.post
+  post: state.post,
 });
-export default connect(
-  mapStateToProps,
-  { getProfileById, getPosts }
-)(Profile);
+export default connect(mapStateToProps, { getProfileById, getPosts })(Profile);
